8fd9e06eb04f55db5b67c71d989cf148e73ca752
August Saint Freytag <me@augustfreytag.com>
2025-06-20 14:40:30 +0200

    Introduce skeleton structure for Fabric mod

---
+++ src/client/java/net/saint/commercialize/CommercializeClient.java
@@ -0,0 +1,13 @@
+package net.saint.commercialize;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.option.KeyBinding;
+
+public class CommercializeClient implements ClientModInitializer {
+	private static KeyBinding openStoreKeyBinding;
+
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
---
+++ src/main/java/net/saint/commercialize/Commercialize.java
@@ -0,0 +1,27 @@
+package net.saint.commercialize;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.fabricmc.api.ModInitializer;
+import net.saint.commercialize.init.Blocks;
+
+public class Commercialize implements ModInitializer {
+
+	// Properties
+
+	public static final String MOD_ID = "commercialize";
+
+	// References
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	// Init
+
+	@Override
+	public void onInitialize() {
+	}
+}
