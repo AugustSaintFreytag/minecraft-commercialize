9e3da3af40052cb61dc586788ca342631fbe404b
August Saint Freytag <me@augustfreytag.com>
2025-06-25 17:01:33 +0200

    Extend localization util to allow pure strings

--- src/main/java/net/saint/commercialize/util/LocalizationUtil.java
+++ src/main/java/net/saint/commercialize/util/LocalizationUtil.java
@@ -1,5 +1,6 @@
 package net.saint.commercialize.util;
 
+import net.minecraft.client.resource.language.I18n;
 import net.minecraft.text.Text;
 import net.saint.commercialize.Commercialize;
 
@@ -8,6 +9,11 @@ public final class LocalizationUtil {
 	public static Text localizedText(String category, String path) {
 		var key = String.format("%s.%s.%s", category, Commercialize.MOD_ID, path);
 		return Text.translatable(key);
+	}
+
+	public static String localizedString(String category, String path) {
+		var key = String.format("%s.%s.%s", category, Commercialize.MOD_ID, path);
+		return I18n.translate(key);
 	}
 
 }
