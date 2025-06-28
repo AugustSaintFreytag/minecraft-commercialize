0f2a8478024f561946777088e01a875ff8ac19c7
August Saint Freytag <me@augustfreytag.com>
2025-06-28 13:38:15 +0200

    Use Identifier type directly in items config

--- src/main/java/net/saint/commercialize/config/ItemsConfig.java
+++ src/main/java/net/saint/commercialize/config/ItemsConfig.java
@@ -3,6 +3,12 @@ package net.saint.commercialize.config;
 import java.util.HashMap;
 import java.util.Map;
 
+import net.minecraft.util.Identifier;
+
 public final class ItemsConfig {
-	public final Map<String, Integer> values = new HashMap<>();
+
+	// Properties
+
+	public final Map<Identifier, Integer> values = new HashMap<>();
+
 }
