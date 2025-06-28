d7f4db5a869a601e4643774e0cbdc6c6cc7a9754
August Saint Freytag <me@augustfreytag.com>
2025-06-28 15:52:34 +0200

    Extend offer template manager with random serving
    Allow manager to pick weighted selection from registered templates.

--- src/main/java/net/saint/commercialize/data/offer/OfferTemplateManager.java
+++ src/main/java/net/saint/commercialize/data/offer/OfferTemplateManager.java
@@ -1,13 +1,22 @@
 package net.saint.commercialize.data.offer;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import net.minecraft.util.math.random.Random;
+import net.saint.commercialize.library.Availability;
 
 public final class OfferTemplateManager {
 
 	// Properties
 
 	private List<OfferTemplate> templates = new ArrayList<>();
+
+	private int[] weightSums;
+	private int weightSum;
+
+	private boolean needsIndexRebuild = false;
 
 	// Access
 
@@ -19,14 +28,51 @@ public final class OfferTemplateManager {
 		return templates;
 	}
 
+	public OfferTemplate getRandomTemplate(Random random) {
+		if (needsIndexRebuild) {
+			rebuildIndex();
+		}
+
+		if (weightSum <= 0) {
+			return null;
+		}
+
+		var randomWeightIndex = random.nextInt(weightSum);
+		var randomIndex = Arrays.binarySearch(weightSums, randomWeightIndex + 1);
+
+		if (randomIndex < 0) {
+			randomIndex = -randomIndex - 1;
+		}
+
+		return templates.get(randomIndex);
+	}
+
 	// Mutation
 
 	public void registerTemplate(OfferTemplate template) {
 		templates.add(template);
+		needsIndexRebuild = true;
 	}
 
 	public void clearTemplates() {
 		templates.clear();
+		needsIndexRebuild = true;
+	}
+
+	public void rebuildIndex() {
+		var n = templates.size();
+
+		weightSums = new int[n];
+		weightSum = 0;
+
+		for (int index = 0; index < n; index++) {
+			var weight = Availability.weightForAvailability(templates.get(index).availability);
+
+			weightSum += weight;
+			weightSums[index] = weightSum;
+		}
+
+		needsIndexRebuild = false;
 	}
 
 }
