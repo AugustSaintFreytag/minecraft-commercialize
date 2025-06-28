53e3686b413f37b2152b58125de59660dfb11b3d
August Saint Freytag <me@augustfreytag.com>
2025-06-28 12:45:33 +0200

    Add type for item value mapping config

---
+++ src/main/java/net/saint/commercialize/config/ItemsConfig.java
@@ -0,0 +1,8 @@
+package net.saint.commercialize.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class ItemsConfig {
+	public final Map<String, Integer> values = new HashMap<>();
+}
