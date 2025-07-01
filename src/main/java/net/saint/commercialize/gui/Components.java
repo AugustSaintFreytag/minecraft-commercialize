54e687d84a0b52b051d4937390f20f4a798c5332
August Saint Freytag <me@augustfreytag.com>
2025-07-01 15:17:15 +0200

    Extend components convenience module
    Add convenience build functions for all available components.

--- src/main/java/net/saint/commercialize/gui/Components.java
+++ src/main/java/net/saint/commercialize/gui/Components.java
@@ -1,12 +1,38 @@
 package net.saint.commercialize.gui;
 
+import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
+import org.jetbrains.annotations.Nullable;
+
+import io.wispforest.owo.ui.component.BlockComponent;
+import io.wispforest.owo.ui.component.BoxComponent;
+import io.wispforest.owo.ui.component.CheckboxComponent;
+import io.wispforest.owo.ui.component.DiscreteSliderComponent;
+import io.wispforest.owo.ui.component.DropdownComponent;
+import io.wispforest.owo.ui.component.EntityComponent;
 import io.wispforest.owo.ui.component.ItemComponent;
 import io.wispforest.owo.ui.component.LabelComponent;
+import io.wispforest.owo.ui.component.SliderComponent;
+import io.wispforest.owo.ui.component.SlimSliderComponent;
+import io.wispforest.owo.ui.component.SmallCheckboxComponent;
+import io.wispforest.owo.ui.component.SpriteComponent;
 import io.wispforest.owo.ui.component.TextureComponent;
+import io.wispforest.owo.ui.component.VanillaWidgetComponent;
+import io.wispforest.owo.ui.container.FlowLayout;
+import io.wispforest.owo.ui.core.Component;
 import io.wispforest.owo.ui.core.Sizing;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.client.gui.widget.ClickableWidget;
+import net.minecraft.client.texture.Sprite;
+import net.minecraft.client.util.SpriteIdentifier;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.saint.commercialize.gui.common.ButtonComponent;
@@ -15,12 +41,40 @@ import net.saint.commercialize.library.TextureReference;
 
 public final class Components {
 
-	public static TextureComponent texture(Identifier texture, int u, int v, int regionWidth, int regionHeight) {
-		return io.wispforest.owo.ui.component.Components.texture(texture, u, v, regionWidth, regionHeight);
+	// Vanilla Components
+
+	public static io.wispforest.owo.ui.component.TextAreaComponent textArea(Sizing horizontalSizing, Sizing verticalSizing) {
+		return io.wispforest.owo.ui.component.Components.textArea(horizontalSizing, verticalSizing);
 	}
 
-	public static TextureComponent texture(TextureReference texture) {
-		return io.wispforest.owo.ui.component.Components.texture(texture.texture, texture.u, texture.v, texture.width, texture.height);
+	public static io.wispforest.owo.ui.component.TextAreaComponent textArea(Sizing horizontalSizing, Sizing verticalSizing, String text) {
+		return io.wispforest.owo.ui.component.Components.textArea(horizontalSizing, verticalSizing, text);
+	}
+
+	// Default Components
+
+	public static <E extends Entity> EntityComponent<E> entity(Sizing sizing, EntityType<E> type, @Nullable NbtCompound nbt) {
+		return io.wispforest.owo.ui.component.Components.entity(sizing, type, nbt);
+	}
+
+	public static <E extends Entity> EntityComponent<E> entity(Sizing sizing, E entity) {
+		return io.wispforest.owo.ui.component.Components.entity(sizing, entity);
+	}
+
+	public static ItemComponent item(ItemStack item) {
+		return io.wispforest.owo.ui.component.Components.item(item);
+	}
+
+	public static BlockComponent block(BlockState state) {
+		return io.wispforest.owo.ui.component.Components.block(state);
+	}
+
+	public static BlockComponent block(BlockState state, BlockEntity blockEntity) {
+		return io.wispforest.owo.ui.component.Components.block(state, blockEntity);
+	}
+
+	public static BlockComponent block(BlockState state, @Nullable NbtCompound nbt) {
+		return io.wispforest.owo.ui.component.Components.block(state, nbt);
 	}
 
 	public static LabelComponent label(Text text) {
@@ -35,8 +89,69 @@ public final class Components {
 		return new ButtonComponent(message, onPress);
 	}
 
-	public static ItemComponent item(ItemStack item) {
-		return io.wispforest.owo.ui.component.Components.item(item);
+	public static CheckboxComponent checkbox(Text message) {
+		return io.wispforest.owo.ui.component.Components.checkbox(message);
+	}
+
+	public static SliderComponent slider(Sizing horizontalSizing) {
+		return io.wispforest.owo.ui.component.Components.slider(horizontalSizing);
+	}
+
+	public static DiscreteSliderComponent discreteSlider(Sizing horizontalSizing, double min, double max) {
+		return io.wispforest.owo.ui.component.Components.discreteSlider(horizontalSizing, min, max);
+	}
+
+	public static SpriteComponent sprite(SpriteIdentifier spriteId) {
+		return io.wispforest.owo.ui.component.Components.sprite(spriteId);
+	}
+
+	public static SpriteComponent sprite(Sprite sprite) {
+		return io.wispforest.owo.ui.component.Components.sprite(sprite);
+	}
+
+	public static TextureComponent texture(TextureReference textureReference) {
+		return io.wispforest.owo.ui.component.Components.texture(textureReference.texture, textureReference.u, textureReference.v,
+				textureReference.width, textureReference.height);
+	}
+
+	public static TextureComponent texture(Identifier texture, int u, int v, int regionWidth, int regionHeight, int textureWidth,
+			int textureHeight) {
+		return io.wispforest.owo.ui.component.Components.texture(texture, u, v, regionWidth, regionHeight, textureWidth, textureHeight);
+	}
+
+	public static TextureComponent texture(Identifier texture, int u, int v, int regionWidth, int regionHeight) {
+		return io.wispforest.owo.ui.component.Components.texture(texture, u, v, regionWidth, regionHeight);
+	}
+
+	public static BoxComponent box(Sizing horizontalSizing, Sizing verticalSizing) {
+		return io.wispforest.owo.ui.component.Components.box(horizontalSizing, verticalSizing);
+	}
+
+	public static DropdownComponent dropdown(Sizing horizontalSizing) {
+		return io.wispforest.owo.ui.component.Components.dropdown(horizontalSizing);
+	}
+
+	public static SlimSliderComponent slimSlider(SlimSliderComponent.Axis axis) {
+		return io.wispforest.owo.ui.component.Components.slimSlider(axis);
+	}
+
+	public static SmallCheckboxComponent smallCheckbox(Text label) {
+		return io.wispforest.owo.ui.component.Components.smallCheckbox(label);
+	}
+
+	// Utility
+
+	public static <T, C extends Component> FlowLayout list(List<T> data, Consumer<FlowLayout> layoutConfigurator,
+			Function<T, C> componentMaker, boolean vertical) {
+		return io.wispforest.owo.ui.component.Components.list(data, layoutConfigurator, componentMaker, vertical);
+	}
+
+	public static VanillaWidgetComponent wrapVanillaWidget(ClickableWidget widget) {
+		return io.wispforest.owo.ui.component.Components.wrapVanillaWidget(widget);
+	}
+
+	public static <T extends Component> T createWithSizing(Supplier<T> componentMaker, Sizing horizontalSizing, Sizing verticalSizing) {
+		return io.wispforest.owo.ui.component.Components.createWithSizing(componentMaker, horizontalSizing, verticalSizing);
 	}
 
 }
