f5135d27a02ff1b918827ded10ccacf3f9871d68
August Saint Freytag <me@augustfreytag.com>
2025-07-04 16:06:54 +0200

    Establish market block and entity logic
    Handle screen inputs, outputs, and network requests.
    Establish round-trip to fetch market manager offers server-side.

--- src/main/java/net/saint/commercialize/blockentity/MarketBlockEntity.java
+++ src/main/java/net/saint/commercialize/blockentity/MarketBlockEntity.java
@@ -1,22 +1,149 @@
 package net.saint.commercialize.blockentity;
 
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.saint.commercialize.Commercialize;
-import net.saint.commercialize.init.Blocks;
+import net.saint.commercialize.data.market.MarketManager;
+import net.saint.commercialize.data.offer.OfferFilterMode;
+import net.saint.commercialize.data.offer.OfferSortMode;
+import net.saint.commercialize.data.offer.OfferSortOrder;
+import net.saint.commercialize.data.payment.PaymentMethod;
+import net.saint.commercialize.init.ModBlocks;
+import net.saint.commercialize.network.MarketC2SQueryMessage;
+import net.saint.commercialize.network.MarketS2CListMessage;
+import net.saint.commercialize.screen.market.MarketScreen;
 
 public class MarketBlockEntity extends BlockEntity {
 
-	// Properties
+	// Configuration
 
 	public static final Identifier ID = new Identifier(Commercialize.MOD_ID, "market_block_entity");
 
+	// Properties
+
+	private MarketManager marketManager = new MarketManager();
+
+	public MarketScreen marketScreen;
+
+	public OfferSortMode sortMode = OfferSortMode.ITEM_NAME;
+	public OfferSortOrder sortOrder = OfferSortOrder.ASCENDING;
+	public OfferFilterMode filterMode = OfferFilterMode.ALL;
+	public PaymentMethod paymentMethod = PaymentMethod.INVENTORY;
+
 	// Init
 
-	public MarketBlockEntity(BlockPos pos, BlockState state) {
-		super(Blocks.MARKET_BLOCK_ENTITY, pos, state);
+	public MarketBlockEntity(BlockPos position, BlockState state) {
+		super(ModBlocks.MARKET_BLOCK_ENTITY, position, state);
+		initNetworking();
+	}
+
+	private void initNetworking() {
+		ClientPlayNetworking.registerReceiver(MarketS2CListMessage.ID, (client, handler, buffer, responseSender) -> {
+			try {
+				var marketData = MarketS2CListMessage.decodeFromBuffer(buffer);
+
+				client.execute(() -> {
+					onReceiveMarketData(marketData);
+				});
+			} catch (Exception e) {
+				Commercialize.LOGGER.error("Could not decode market data received from server.", e);
+				return;
+			}
+		});
+	}
+
+	@Override
+	public void readNbt(NbtCompound nbt) {
+		// TODO Auto-generated method stub
+		super.readNbt(nbt);
+	}
+
+	@Override
+	protected void writeNbt(NbtCompound nbt) {
+		// TODO Auto-generated method stub
+		super.writeNbt(nbt);
+	}
+
+	// Screen
+
+	public void openMarketScreen(World world, PlayerEntity player) {
+		if (!world.isClient()) {
+			return;
+		}
+
+		var client = MinecraftClient.getInstance();
+
+		this.marketScreen = new MarketScreen();
+
+		this.marketScreen.onUpdate = this::onMarketScreenUpdate;
+		this.marketScreen.sortMode = sortMode;
+		this.marketScreen.sortOrder = sortOrder;
+		this.marketScreen.filterMode = filterMode;
+		this.marketScreen.paymentMethod = paymentMethod;
+
+		marketScreen.offers = marketManager.getOffers().toList();
+
+		client.setScreen(marketScreen);
+
+		requestMarketData();
+	}
+
+	private void onReceiveMarketData(MarketS2CListMessage marketData) {
+		marketManager.clearOffers();
+		marketManager.addOffers(marketData.offers);
+
+		Commercialize.LOGGER.info("Received market data for market block entity at pos '{}' from server: {} offer(s) available.",
+				this.getPos().toShortString(), marketManager.getOffers().count());
+
+		updateMarketScreen();
+	}
+
+	private void onMarketScreenUpdate() {
+		if (this.marketScreen == null) {
+			Commercialize.LOGGER.warn("Can not process market screen update, missing screen reference.");
+			return;
+		}
+
+		this.filterMode = this.marketScreen.filterMode;
+		this.sortMode = this.marketScreen.sortMode;
+		this.sortOrder = this.marketScreen.sortOrder;
+		this.paymentMethod = this.marketScreen.paymentMethod;
+
+		this.requestMarketData();
+	}
+
+	private void updateMarketScreen() {
+		if (this.marketScreen == null) {
+			Commercialize.LOGGER.warn(
+					"Can not update market screen, missing screen reference. Update likely requested for inactive block or incorrect receiver.");
+			return;
+		}
+
+		this.marketScreen.offers = marketManager.getOffers().toList();
+		this.marketScreen.updateDisplay();
+	}
+
+	// Networking
+
+	public void requestMarketData() {
+		var message = new MarketC2SQueryMessage();
+
+		message.sortMode = sortMode;
+		message.sortOrder = sortOrder;
+		message.filterMode = filterMode;
+
+		var buffer = PacketByteBufs.create();
+		message.encodeToBuffer(buffer);
+
+		ClientPlayNetworking.send(MarketC2SQueryMessage.ID, buffer);
 	}
 
 }
