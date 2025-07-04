94ddd96b5dc376931e39e3abfd8f548f5e4f49a0
August Saint Freytag <me@augustfreytag.com>
2025-07-04 15:41:12 +0200

    Add mutation functionality to market manager

--- src/main/java/net/saint/commercialize/data/market/MarketManager.java
+++ src/main/java/net/saint/commercialize/data/market/MarketManager.java
@@ -1,7 +1,9 @@
 package net.saint.commercialize.data.market;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Stream;
 
 import net.saint.commercialize.data.offer.Offer;
@@ -26,6 +28,22 @@ public final class MarketManager {
 
 	public void addOffer(Offer offer) {
 		offers.add(offer);
+	}
+
+	public void removeOffer(Offer offer) {
+		offers.remove(offer);
+	}
+
+	public void removeOffer(UUID id) {
+		offers.removeIf(offer -> offer.id.equals(id));
+	}
+
+	public void removeOffers(Collection<Offer> offers) {
+		this.offers.removeAll(offers);
+	}
+
+	public void addOffers(Collection<Offer> offers) {
+		this.offers.addAll(offers);
 	}
 
 	public void clearOffers() {
