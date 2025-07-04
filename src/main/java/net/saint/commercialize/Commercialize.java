26f6a50bbf486911e20bb1e49c864fe048118882
August Saint Freytag <me@augustfreytag.com>
2025-07-04 16:08:52 +0200

    Introduce market offer ticking in dedicated util.
    Remove debug code, clean up mod entry point module.

--- src/main/java/net/saint/commercialize/Commercialize.java
+++ src/main/java/net/saint/commercialize/Commercialize.java
@@ -3,18 +3,17 @@ package net.saint.commercialize;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
-import net.minecraft.world.World;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.saint.commercialize.data.item.ItemManager;
 import net.saint.commercialize.data.market.MarketManager;
-import net.saint.commercialize.data.market.MarketOfferGenerator;
 import net.saint.commercialize.data.offer.OfferTemplateManager;
 import net.saint.commercialize.init.ModBlocks;
+import net.saint.commercialize.init.ModCommands;
 import net.saint.commercialize.init.ModNetworking;
 import net.saint.commercialize.util.ConfigLoadUtil;
+import net.saint.commercialize.util.MarketOfferTickingUtil;
 import net.saint.commercialize.util.PlayerProfileManager;
 
 public class Commercialize implements ModInitializer {
@@ -33,24 +32,33 @@ public class Commercialize implements ModInitializer {
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 
+	public static boolean shouldTickMarket = true;
+
 	// Init
 
 	@Override
 	public void onInitialize() {
 		ModBlocks.initialize();
 		ModNetworking.initialize();
+		ModCommands.initialize();
 
 		ServerLifecycleEvents.SERVER_STARTED.register(server -> {
 			loadItemConfigs();
 			loadPlayersConfig();
 			loadOfferTemplatesConfigs();
+		});
 
-			generateDemoOffers(server.getOverworld());
+		ServerTickEvents.END_SERVER_TICK.register(server -> {
+			if (!shouldTickMarket) {
+				return;
+			}
+
+			var world = server.getOverworld();
+			MarketOfferTickingUtil.tickMarketOffersIfNecessary(world);
 		});
 
 	}
 
-	@Environment(EnvType.SERVER)
 	private void loadItemConfigs() {
 		var itemConfigs = ConfigLoadUtil.loadItemConfigs();
 
@@ -63,7 +71,6 @@ public class Commercialize implements ModInitializer {
 		Commercialize.LOGGER.info("Loaded {} item configs with a total of {} item(s).", itemConfigs.size(), ITEM_MANAGER.size());
 	}
 
-	@Environment(EnvType.SERVER)
 	private void loadPlayersConfig() {
 		var playersConfig = ConfigLoadUtil.loadPlayersConfig();
 		PLAYER_PROFILE_MANAGER.registerReferencePlayerNames(playersConfig.players);
@@ -71,7 +78,6 @@ public class Commercialize implements ModInitializer {
 		Commercialize.LOGGER.info("Loaded {} mock player names.", PLAYER_PROFILE_MANAGER.numberOfReferencePlayerNames());
 	}
 
-	@Environment(EnvType.SERVER)
 	private void loadOfferTemplatesConfigs() {
 		var offerTemplateConfigs = ConfigLoadUtil.loadOfferTemplateConfigs();
 
@@ -85,20 +91,4 @@ public class Commercialize implements ModInitializer {
 				OFFER_TEMPLATE_MANAGER.size());
 	}
 
-	@Environment(EnvType.SERVER)
-	private void generateDemoOffers(World world) {
-		MARKET_MANAGER.clearOffers();
-
-		for (int i = 0; i < 32; i++) {
-			var offer = MarketOfferGenerator.generateOffer(world);
-
-			if (offer.isEmpty()) {
-				continue;
-			}
-
-			MARKET_MANAGER.addOffer(offer.get());
-		}
-
-		Commercialize.LOGGER.info("Generated {} market offer(s) for world '{}'.", MARKET_MANAGER.size(), world.getRegistryKey().getValue());
-	}
 }
