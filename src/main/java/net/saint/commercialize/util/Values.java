130f6edfd210e52fab0ec52561a57053215b7808
August Saint Freytag <me@augustfreytag.com>
2025-07-14 00:35:06 +0200

    Establish non-returning if present value func.

--- src/main/java/net/saint/commercialize/util/Values.java
+++ src/main/java/net/saint/commercialize/util/Values.java
@@ -1,18 +1,29 @@
 package net.saint.commercialize.util;
 
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 import net.minecraft.nbt.NbtCompound;
 
 public final class Values {
 
-	public static <T, U> U ifPresent(T value, Function<? super T, ? extends U> action, U fallback) {
+	// General
+
+	public static <T> void ifPresent(T value, Consumer<? super T> action) {
+		if (value != null) {
+			action.accept(value);
+		}
+	}
+
+	public static <T, U> U returnIfPresent(T value, Function<? super T, ? extends U> action, U fallback) {
 		if (value != null) {
 			return action.apply(value);
 		}
 
 		return fallback;
 	}
+
+	// NBT
 
 	public static <U> U ifPresentAsString(NbtCompound nbt, String key, Function<String, ? extends U> action, U fallback) {
 		if (nbt.contains(key)) {
