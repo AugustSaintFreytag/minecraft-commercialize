5109185a98060d70c44cefa56be0e4bcdad48fdc
August Saint Freytag <me@augustfreytag.com>
2025-07-27 12:58:35 +0200

    Refactor double height block into abstract base
    Allow double height set-up and internal logic to be reused.

--- src/main/java/net/saint/commercialize/block/shipping/ShippingBlock.java
+++ src/main/java/net/saint/commercialize/block/shipping/ShippingBlock.java
@@ -3,23 +3,17 @@ package net.saint.commercialize.block.shipping;
 import java.util.HashMap;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.BlockWithEntity;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.block.enums.DoubleBlockHalf;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
-import net.minecraft.item.ItemStack;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.state.StateManager.Builder;
 import net.minecraft.state.property.DirectionProperty;
-import net.minecraft.state.property.EnumProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.BlockMirror;
@@ -34,19 +28,18 @@ import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldEvents;
 import net.saint.commercialize.Commercialize;
+import net.saint.commercialize.block.DoubleBlockWithEntity;
 import net.saint.commercialize.init.ModBlockEntities;
 import net.saint.commercialize.util.VoxelShapeUtil;
 
-public class ShippingBlock extends BlockWithEntity {
+public class ShippingBlock extends DoubleBlockWithEntity {
 
 	// Properties
 
 	public static final Identifier ID = new Identifier(Commercialize.MOD_ID, "shipping_block");
 
 	public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
-	public static final EnumProperty<DoubleBlockHalf> HALF = Properties.DOUBLE_BLOCK_HALF;
 
 	private static final HashMap<DoubleBlockHalf, VoxelShape> SHAPES = new HashMap<>() {
 		{
@@ -68,7 +61,7 @@ public class ShippingBlock extends BlockWithEntity {
 		super(settings);
 	}
 
-	// Block
+	// Entity
 
 	@Override
 	public BlockEntity createBlockEntity(BlockPos position, BlockState blockState) {
@@ -79,43 +72,17 @@ public class ShippingBlock extends BlockWithEntity {
 		return new ShippingBlockEntity(position, blockState);
 	}
 
-	@Override
-	public BlockRenderType getRenderType(BlockState state) {
-		return BlockRenderType.MODEL;
-	}
+	// Placement
 
 	@Override
-	public BlockState getPlacementState(ItemPlacementContext context) {
-		var blockPos = context.getBlockPos();
-		var world = context.getWorld();
-
-		if (blockPos.getY() < world.getTopY() - 1 && world.getBlockState(blockPos.up()).canReplace(context)) {
-			return this.getDefaultState().with(FACING, context.getHorizontalPlayerFacing().getOpposite()).with(HALF, DoubleBlockHalf.LOWER);
-		} else {
-			return null;
-		}
-	}
-
-	@Override
-	public VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-		return SHAPES_BY_DIRECTION.get(state.get(HALF)).get(state.get(FACING));
-	}
-
-	@Override
-	public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-		return SHAPES_BY_DIRECTION.get(state.get(HALF)).get(state.get(FACING));
-	}
-
-	@Override
-	public void onPlaced(World world, BlockPos position, BlockState state, LivingEntity placer, ItemStack itemStack) {
-		world.setBlockState(position.up(), state.with(HALF, DoubleBlockHalf.UPPER), Block.NOTIFY_ALL);
-		super.onPlaced(world, position, state, placer, itemStack);
+	protected BlockState getMasterPlacementState(ItemPlacementContext context) {
+		return this.getDefaultState().with(FACING, context.getHorizontalPlayerFacing().getOpposite());
 	}
 
 	@Override
 	protected void appendProperties(Builder<Block, BlockState> builder) {
 		builder.add(FACING);
-		builder.add(HALF);
+		super.appendProperties(builder);
 	}
 
 	@Override
@@ -128,67 +95,31 @@ public class ShippingBlock extends BlockWithEntity {
 		return state.rotate(mirror.getRotation(state.get(FACING)));
 	}
 
-	// World Mutation
+	// Shape
 
 	@Override
-	public void onBreak(World world, BlockPos position, BlockState state, PlayerEntity player) {
-		var twinBlockPosition = getTwinBlockPosition(position, state);
-		var twinBlockState = world.getBlockState(twinBlockPosition);
-
-		if (twinBlockState.getBlock() == this) {
-			world.setBlockState(twinBlockPosition, Blocks.AIR.getDefaultState(), Block.NOTIFY_ALL | Block.SKIP_DROPS);
-			world.syncWorldEvent(player, WorldEvents.BLOCK_BROKEN, twinBlockPosition, Block.getRawIdFromState(twinBlockState));
-		}
-
-		super.onBreak(world, position, state, player);
+	public VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+		return SHAPES_BY_DIRECTION.get(state.get(HALF)).get(state.get(FACING));
 	}
 
 	@Override
-	public void onStateReplaced(BlockState state, World world, BlockPos position, BlockState newState, boolean moved) {
-		if (state.get(HALF) == DoubleBlockHalf.UPPER) {
-			return;
-		}
+	public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+		return SHAPES_BY_DIRECTION.get(state.get(HALF)).get(state.get(FACING));
+	}
 
-		if (state.getBlock() == newState.getBlock()) {
-			return;
-		}
+	// Mutation
 
+	@Override
+	protected void onStateReplacedLowerHalf(BlockState state, World world, BlockPos position, BlockState newState, boolean moved) {
 		var blockEntity = (ShippingBlockEntity) world.getBlockEntity(position);
 		ItemScatterer.spawn(world, position, blockEntity);
-	}
-
-	public long getRenderingSeed(BlockState state, BlockPos position) {
-		var x = position.getX();
-		var y = position.down(state.get(HALF) == DoubleBlockHalf.LOWER ? 0 : 1).getY();
-		var z = position.getZ();
-
-		return new BlockPos(x, y, z).hashCode();
-	}
-
-	// Ticker
-
-	@Override
-	public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
-		return checkType(type, ModBlockEntities.SHIPPING_BLOCK_ENTITY, ShippingBlockEntity::tick);
 	}
 
 	// Interaction
 
 	@Override
-	public ActionResult onUse(BlockState state, World world, BlockPos position, PlayerEntity player, Hand hand, BlockHitResult hit) {
-		if (world.isClient() || hand == Hand.OFF_HAND) {
-			return ActionResult.CONSUME;
-		}
-
-		if (state.get(HALF) == DoubleBlockHalf.UPPER) {
-			// Interaction with upper half is deferred to lower half instead.
-			var lowerHalfPosition = position.down();
-			var lowerHalfBlockState = world.getBlockState(lowerHalfPosition);
-			var lowerHalfBlock = (ShippingBlock) lowerHalfBlockState.getBlock();
-
-			return lowerHalfBlock.onUse(lowerHalfBlockState, world, lowerHalfPosition, player, hand, hit);
-		}
-
+	protected ActionResult onMasterBlockUse(BlockState state, World world, BlockPos position, PlayerEntity player, Hand hand,
+			BlockHitResult hit) {
 		var screenHandlerFactory = state.createScreenHandlerFactory(world, position);
 
 		if (screenHandlerFactory == null) {
@@ -199,6 +130,15 @@ public class ShippingBlock extends BlockWithEntity {
 		return ActionResult.SUCCESS;
 	}
 
+	// Ticker
+
+	@Override
+	public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
+		return checkType(type, ModBlockEntities.SHIPPING_BLOCK_ENTITY, ShippingBlockEntity::tick);
+	}
+
+	// Redstone
+
 	@Override
 	public boolean hasComparatorOutput(BlockState state) {
 		return true;
@@ -207,16 +147,6 @@ public class ShippingBlock extends BlockWithEntity {
 	@Override
 	public int getComparatorOutput(BlockState state, World world, BlockPos pos) {
 		return ScreenHandler.calculateComparatorOutput(world.getBlockEntity(pos));
-	}
-
-	// Utility
-
-	private BlockPos getTwinBlockPosition(BlockPos position, BlockState state) {
-		if (state.get(HALF) == DoubleBlockHalf.LOWER) {
-			return position.up();
-		} else {
-			return position.down();
-		}
 	}
 
 }
