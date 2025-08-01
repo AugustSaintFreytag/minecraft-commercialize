c3069172aac394fd0fb2f0fd28ed0eac578dbe37
August Saint Freytag <me@augustfreytag.com>
2025-08-01 23:37:58 +0200

    Reformat shipping block entity switch statement

--- src/main/java/net/saint/commercialize/block/shipping/ShippingBlockEntity.java
+++ src/main/java/net/saint/commercialize/block/shipping/ShippingBlockEntity.java
@@ -90,16 +90,16 @@ public class ShippingBlockEntity extends BlockEntity implements ImplementedInven
 
 		ShippingExchangeTickingUtil.tickShippingIfNecessary(world, blockEntity, result -> {
 			switch (result) {
-			case SOLD:
-				world.playSound(null, blockEntity.getPos(), ModSounds.MAILBOX_DELIVERY_SOUND, SoundCategory.BLOCKS, 1.5f, 0.75f);
-				break;
-			case NO_ITEMS:
-				// No feedback sound when not selling items.
-				// world.playSound(null, blockEntity.getPos(), SoundEvents.BLOCK_NOTE_BLOCK_BASS.value(), SoundCategory.BLOCKS, 0.5f, 0.25f);
-				break;
-			case FAILURE:
-				world.playSound(null, blockEntity.getPos(), SoundEvents.BLOCK_NOTE_BLOCK_BASS.value(), SoundCategory.BLOCKS, 1f, 0.5f);
-				break;
+				case SOLD:
+					world.playSound(null, blockEntity.getPos(), ModSounds.MAILBOX_DELIVERY_SOUND, SoundCategory.BLOCKS, 1.5f, 0.75f);
+					break;
+				case NO_ITEMS:
+					// No feedback sound when not selling items.
+					// world.playSound(null, blockEntity.getPos(), SoundEvents.BLOCK_NOTE_BLOCK_BASS.value(), SoundCategory.BLOCKS, 0.5f, 0.25f);
+					break;
+				case FAILURE:
+					world.playSound(null, blockEntity.getPos(), SoundEvents.BLOCK_NOTE_BLOCK_BASS.value(), SoundCategory.BLOCKS, 1f, 0.5f);
+					break;
 			}
 		});
 	}
