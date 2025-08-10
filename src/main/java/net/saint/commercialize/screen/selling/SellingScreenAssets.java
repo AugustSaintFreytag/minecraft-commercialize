dc1b48d888fdd569dbe87d2cfb22302e474c400f
August Saint Freytag <me@augustfreytag.com>
2025-08-10 14:39:46 +0200

    Introduce selling screen GUI texture and catalog

---
+++ src/main/java/net/saint/commercialize/screen/selling/SellingScreenAssets.java
@@ -0,0 +1,17 @@
+package net.saint.commercialize.screen.selling;
+
+import net.minecraft.util.Identifier;
+import net.saint.commercialize.Commercialize;
+import net.saint.commercialize.library.TextureReference;
+
+public final class SellingScreenAssets {
+
+	// Configuration
+
+	public static final Identifier TEXTURE = new Identifier(Commercialize.MOD_ID, "textures/gui/selling.png");
+
+	// Panel
+
+	public static final TextureReference PANEL = new TextureReference(TEXTURE, 0, 0, 215, 215);
+
+}
