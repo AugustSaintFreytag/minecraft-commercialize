66e5af56733e780e565bef1775cdee4538b8b6f3
August Saint Freytag <me@augustfreytag.com>
2025-08-11 14:09:27 +0200

    Fix time preset values for short intervals

--- src/main/java/net/saint/commercialize/data/text/TimePreset.java
+++ src/main/java/net/saint/commercialize/data/text/TimePreset.java
@@ -5,9 +5,9 @@ public final class TimePreset {
 	private static final int NUMBER_OF_TICKS_PER_DAY = 24_000;
 
 	public static final long ONE_HOUR = NUMBER_OF_TICKS_PER_DAY / 24;
-	public static final long TWO_HOURS = NUMBER_OF_TICKS_PER_DAY * 2;
-	public static final long SIX_HOURS = NUMBER_OF_TICKS_PER_DAY * 6;
-	public static final long TWELVE_HOURS = NUMBER_OF_TICKS_PER_DAY * 12;
+	public static final long TWO_HOURS = ONE_HOUR * 2;
+	public static final long SIX_HOURS = ONE_HOUR * 6;
+	public static final long TWELVE_HOURS = ONE_HOUR * 12;
 
 	public static final long ONE_DAY = NUMBER_OF_TICKS_PER_DAY;
 	public static final long THREE_DAYS = NUMBER_OF_TICKS_PER_DAY * 3;
