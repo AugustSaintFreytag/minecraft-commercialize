545d0385c4c740dc4da81ccf9bd97ca19d92fdd8
August Saint Freytag <me@augustfreytag.com>
2025-08-15 15:07:35 +0200

    Add collision/outline shape for market block

--- src/main/java/net/saint/commercialize/block/market/MarketBlock.java
+++ src/main/java/net/saint/commercialize/block/market/MarketBlock.java
@@ -1,7 +1,10 @@
 package net.saint.commercialize.block.market;
 
+import java.util.HashMap;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
@@ -19,10 +22,15 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.saint.commercialize.Commercialize;
 import net.saint.commercialize.block.DoubleBlockWithEntity;
 import net.saint.commercialize.init.ModBlockEntities;
+import net.saint.commercialize.util.VoxelShapeUtil;
 
 public class MarketBlock extends DoubleBlockWithEntity {
 
@@ -32,10 +40,56 @@ public class MarketBlock extends DoubleBlockWithEntity {
 
 	public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
 
+	private static final HashMap<DoubleBlockHalf, VoxelShape> SHAPES = new HashMap<>() {
+		{
+			this.put(DoubleBlockHalf.LOWER,
+					VoxelShapes.union(VoxelShapes.cuboid(0.1875, 0.0625, 0.1875, 0.8125, 0.125, 0.8125),
+							VoxelShapes.cuboid(0.0625, 0, 0.0625, 0.9375, 0.0625, 0.9375),
+							VoxelShapes.cuboid(0.3125, 0.0625, 0.3125, 0.6875, 0.75, 0.6875),
+							VoxelShapes.cuboid(0.0625, 0.75, 0.0625, 0.9375, 1, 0.9375)));
+			this.put(DoubleBlockHalf.UPPER,
+					VoxelShapes.union(VoxelShapes.cuboid(0.875, 0.8125, 0.0625, 0.9375, 0.9375, 0.3125),
+							VoxelShapes.cuboid(0.875, 0.6875, 0.125, 0.9375, 0.8125, 0.375),
+							VoxelShapes.cuboid(0.875, 0.5625, 0.1875, 0.9375, 0.6875, 0.3125),
+							VoxelShapes.cuboid(0.875, 0.4375, 0.25, 0.9375, 0.5625, 0.3125),
+							VoxelShapes.cuboid(0.0625, 0.8125, 0.0625, 0.125, 0.9375, 0.3125),
+							VoxelShapes.cuboid(0.0625, 0.6875, 0.125, 0.125, 0.8125, 0.3125),
+							VoxelShapes.cuboid(0.0625, 0.5625, 0.1875, 0.125, 0.6875, 0.3125),
+							VoxelShapes.cuboid(0.0625, 0.4375, 0.25, 0.125, 0.5625, 0.3125),
+							VoxelShapes.cuboid(0.125, 0.875, 0.3125, 0.875, 0.9375, 0.375),
+							VoxelShapes.cuboid(0.125, 0.1875, 0.3125, 0.875, 0.3125, 0.375),
+							VoxelShapes.cuboid(0.0625, 0.1875, 0.3125, 0.125, 0.9375, 0.375),
+							VoxelShapes.cuboid(0.875, 0.1875, 0.3125, 0.9375, 0.9375, 0.375),
+							VoxelShapes.cuboid(0.125, 0.1875, 0.125, 0.875, 0.25, 0.3125),
+							VoxelShapes.cuboid(0.0625, 0.9375, 0.0625, 0.9375, 1, 0.9375),
+							VoxelShapes.cuboid(0.0625, 0.1875, 0.375, 0.9375, 0.9375, 0.9375),
+							VoxelShapes.cuboid(0.0625, 0, 0.0625, 0.9375, 0.1875, 0.9375)));
+		}
+	};
+
+	private static final HashMap<DoubleBlockHalf, HashMap<Direction, VoxelShape>> SHAPES_BY_DIRECTION = new HashMap<>() {
+		{
+			this.put(DoubleBlockHalf.LOWER, VoxelShapeUtil.createDirectionalShapes(SHAPES.get(DoubleBlockHalf.LOWER)));
+			this.put(DoubleBlockHalf.UPPER, VoxelShapeUtil.createDirectionalShapes(SHAPES.get(DoubleBlockHalf.UPPER)));
+		}
+	};
+
 	// Init
 
 	public MarketBlock(Settings settings) {
 		super(settings);
+	}
+
+	// Shape
+
+	@Override
+	public VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+		return SHAPES_BY_DIRECTION.get(state.get(HALF)).get(state.get(FACING));
+	}
+
+	@Override
+	public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+		return SHAPES_BY_DIRECTION.get(state.get(HALF)).get(state.get(FACING));
 	}
 
 	// Entity
