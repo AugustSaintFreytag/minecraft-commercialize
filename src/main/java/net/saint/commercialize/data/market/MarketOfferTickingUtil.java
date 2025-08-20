e24930e569ebd3ab9e18eb6786c63a4a2279a5a0
August Saint Freytag <me@augustfreytag.com>
2025-08-20 14:57:27 +0200

    Introduce offer item returns for expired offers

--- src/main/java/net/saint/commercialize/data/market/MarketOfferTickingUtil.java
+++ src/main/java/net/saint/commercialize/data/market/MarketOfferTickingUtil.java
@@ -1,10 +1,16 @@
 package net.saint.commercialize.data.market;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.world.World;
 import net.saint.commercialize.Commercialize;
+import net.saint.commercialize.data.mail.MailTransitUtil;
 import net.saint.commercialize.data.offer.Offer;
+import net.saint.commercialize.data.text.ItemDescriptionUtil;
+import net.saint.commercialize.util.LocalizationUtil;
 
 public final class MarketOfferTickingUtil {
 
@@ -37,16 +43,53 @@ public final class MarketOfferTickingUtil {
 	}
 
 	public static void tickMarketOfferExpiration(World world) {
-		var expiredOffers = new ArrayList<Offer>();
-
-		Commercialize.MARKET_OFFER_MANAGER.getOffers().filter(offer -> (world.getTimeOfDay() - offer.timestamp) >= offer.duration)
-				.forEach(offer -> {
-					expiredOffers.add(offer);
-				});
-
-		expiredOffers.forEach(offer -> {
-			Commercialize.MARKET_OFFER_MANAGER.removeOffer(offer);
+		Commercialize.MARKET_OFFER_MANAGER.getOffers().filter(offer -> {
+			return (world.getTimeOfDay() - offer.timestamp) >= offer.duration;
+		}).collect(Collectors.toCollection(ArrayList::new)).forEach(offer -> {
+			expireAndRemoveOffer(world, offer);
 		});
+	}
+
+	private static void expireAndRemoveOffer(World world, Offer offer) {
+		if (!offer.isGenerated) {
+			expireAndRemovePlayerOffer(world, offer);
+		} else {
+			expireAndRemoveGeneratedOffer(world, offer);
+		}
+	}
+
+	private static void expireAndRemovePlayerOffer(World world, Offer offer) {
+		// Offer is owned by real player, send items back to player mailbox.
+		var itemList = DefaultedList.ofSize(1, ItemStack.EMPTY);
+		itemList.set(0, offer.stack);
+
+		var itemDescription = ItemDescriptionUtil.descriptionForItemStack(offer.stack);
+		var packageSender = LocalizationUtil.localizedString("text", "delivery.market");
+		var packageMessage = LocalizationUtil.localizedString("text", "return.message", itemDescription);
+
+		var server = world.getServer();
+		var player = MarketPlayerUtil.playerEntityForId(server, offer.sellerId);
+
+		if (player == null) {
+			Commercialize.LOGGER.error("Could not find player '%s' (%s) to return expired offer items.", offer.sellerName, offer.sellerId);
+			return;
+		}
+
+		var didDispatch = MailTransitUtil.packageAndDispatchItemStacksToPlayer(server, player, itemList, packageMessage, packageSender);
+
+		if (!didDispatch) {
+			Commercialize.LOGGER.error("Could not dispatch return expired offer items to player '%s' (%s).", offer.sellerName,
+					offer.sellerId);
+			return;
+		}
+
+		Commercialize.LOGGER.info("Offer '%s' (%s) has expired and items were dispatched to seller '%s' (%s).", offer.id, itemDescription,
+				offer.sellerName, offer.sellerId);
+		Commercialize.MARKET_OFFER_MANAGER.removeOffer(offer);
+	}
+
+	private static void expireAndRemoveGeneratedOffer(World world, Offer offer) {
+		Commercialize.MARKET_OFFER_MANAGER.removeOffer(offer);
 	}
 
 }
