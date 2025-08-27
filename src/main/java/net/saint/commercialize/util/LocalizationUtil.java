05d1142c0128386477e879e256b0e817ef2351f3
August Saint Freytag <me@augustfreytag.com>
2025-08-27 16:24:32 +0200

    Displace translation key formulation to own func.

--- src/main/java/net/saint/commercialize/util/LocalizationUtil.java
+++ src/main/java/net/saint/commercialize/util/LocalizationUtil.java
@@ -6,13 +6,17 @@ import net.saint.commercialize.Commercialize;
 
 public final class LocalizationUtil {
 
+	public static String key(String category, String path) {
+		return String.format("%s.%s.%s", category, Commercialize.MOD_ID, path);
+	}
+
 	public static Text localizedText(String category, String path, Object... args) {
-		var key = String.format("%s.%s.%s", category, Commercialize.MOD_ID, path);
+		var key = key(category, path);
 		return Text.translatable(key, args);
 	}
 
 	public static String localizedString(String category, String path, Object... args) {
-		var key = String.format("%s.%s.%s", category, Commercialize.MOD_ID, path);
+		var key = key(category, path);
 		return I18n.translate(key, args);
 	}
 
