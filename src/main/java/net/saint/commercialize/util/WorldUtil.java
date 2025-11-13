2a39d8551d6922bb7c0ecbc57a4cc4c3bcee7a96
August Saint Freytag <me@augustfreytag.com>
2025-11-13 20:31:41 +0100

    Introduce world utils for loaded chunk iteration

---
+++ src/main/java/net/saint/commercialize/util/WorldUtil.java
@@ -0,0 +1,35 @@
+package net.saint.commercialize.util;
+
+import java.util.function.Consumer;
+
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.WorldChunk;
+
+public final class WorldUtil {
+
+	public static void forEachChunkAroundPosition(World world, BlockPos position, int radiusInChunks, Consumer<WorldChunk> consumer) {
+		var chunkX = position.getX() >> 4;
+		var chunkZ = position.getZ() >> 4;
+
+		for (var dx = -radiusInChunks; dx <= radiusInChunks; dx++) {
+			for (var dz = -radiusInChunks; dz <= radiusInChunks; dz++) {
+				var currentChunkX = chunkX + dx;
+				var currentChunkZ = chunkZ + dz;
+				var chunk = world.getChunk(currentChunkX, currentChunkZ);
+
+				consumer.accept(chunk);
+			}
+		}
+	}
+
+	public static <T extends BlockEntity> void forEachBlockEntityInChunk(WorldChunk chunk, Class<T> type, Consumer<T> consumer) {
+		chunk.getBlockEntities().values().forEach(blockEntity -> {
+			if (type.isInstance(blockEntity)) {
+				consumer.accept(type.cast(blockEntity));
+			}
+		});
+	}
+
+}
