968f15db6693d0706e213703edfcf39664fc019f
August Saint Freytag <me@augustfreytag.com>
2025-11-19 00:27:48 +0100

    Introduce accessor for bucket item fluid property

---
+++ src/main/java/net/saint/commercialize/mixin/BucketItemAccessor.java
@@ -0,0 +1,15 @@
+package net.saint.commercialize.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import net.minecraft.fluid.Fluid;
+import net.minecraft.item.BucketItem;
+
+@Mixin(BucketItem.class)
+public interface BucketItemAccessor {
+
+	@Accessor("fluid")
+	Fluid commercialize$getFluid();
+
+}
