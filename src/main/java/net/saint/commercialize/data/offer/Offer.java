8f60854aa9c41ca08330640ec9589698c0f130ed
August Saint Freytag <me@augustfreytag.com>
2025-12-18 03:34:00 +0100

    Add "fees" property to offer data model
    Clean and streamline all NBT keys on model.

--- src/main/java/net/saint/commercialize/data/offer/Offer.java
+++ src/main/java/net/saint/commercialize/data/offer/Offer.java
@@ -8,7 +8,20 @@ import net.minecraft.network.PacketByteBuf;
 
 public class Offer {
 
+	// Configuration
+
 	public static final UUID GENERATED_SELLER_ID = new UUID(0, 0);
+
+	private static final String ID_NBT_KEY = "Id";
+	private static final String IS_ACTIVE_NBT_KEY = "IsActive";
+	private static final String IS_GENERATED_NBT_KEY = "IsGenerated";
+	private static final String SELLER_ID_NBT_KEY = "SellerId";
+	private static final String SELLER_NAME_NBT_KEY = "SellerName";
+	private static final String TIMESTAMP_NBT_KEY = "Timestamp";
+	private static final String DURATION_NBT_KEY = "Duration";
+	private static final String STACK_NBT_KEY = "Stack";
+	private static final String PRICE_NBT_KEY = "Price";
+	private static final String FEES_NBT_KEY = "Fees";
 
 	// Properties
 
@@ -25,6 +38,7 @@ public class Offer {
 
 	public ItemStack stack;
 	public int price;
+	public int fees;
 
 	// Decoding
 
@@ -40,6 +54,7 @@ public class Offer {
 		offer.duration = buffer.readLong();
 		offer.stack = buffer.readItemStack();
 		offer.price = buffer.readInt();
+		offer.fees = buffer.readInt();
 
 		return offer;
 	}
@@ -47,15 +62,16 @@ public class Offer {
 	public static Offer fromNBT(NbtCompound nbt) {
 		var offer = new Offer();
 
-		offer.id = nbt.getUuid("id");
-		offer.isActive = nbt.getBoolean("isActive");
-		offer.isGenerated = nbt.getBoolean("isGenerated");
-		offer.sellerId = nbt.getUuid("sellerId");
-		offer.sellerName = nbt.getString("sellerName");
-		offer.timestamp = nbt.getLong("timestamp");
-		offer.duration = nbt.getLong("duration");
-		offer.stack = ItemStack.fromNbt(nbt.getCompound("stack"));
-		offer.price = nbt.getInt("price");
+		offer.id = nbt.getUuid(ID_NBT_KEY);
+		offer.isActive = nbt.getBoolean(IS_ACTIVE_NBT_KEY);
+		offer.isGenerated = nbt.getBoolean(IS_GENERATED_NBT_KEY);
+		offer.sellerId = nbt.getUuid(SELLER_ID_NBT_KEY);
+		offer.sellerName = nbt.getString(SELLER_NAME_NBT_KEY);
+		offer.timestamp = nbt.getLong(TIMESTAMP_NBT_KEY);
+		offer.duration = nbt.getLong(DURATION_NBT_KEY);
+		offer.stack = ItemStack.fromNbt(nbt.getCompound(STACK_NBT_KEY));
+		offer.price = nbt.getInt(PRICE_NBT_KEY);
+		offer.fees = nbt.getInt(FEES_NBT_KEY);
 
 		return offer;
 	}
@@ -72,18 +88,20 @@ public class Offer {
 		buffer.writeLong(duration);
 		buffer.writeItemStack(stack);
 		buffer.writeInt(price);
+		buffer.writeInt(fees);
 	}
 
 	public NbtCompound writeNbt(NbtCompound nbt) {
-		nbt.putUuid("id", id);
-		nbt.putBoolean("isActive", isActive);
-		nbt.putBoolean("isGenerated", isGenerated);
-		nbt.putUuid("sellerId", sellerId);
-		nbt.putString("sellerName", sellerName);
-		nbt.putLong("timestamp", timestamp);
-		nbt.putLong("duration", duration);
-		nbt.put("stack", stack.writeNbt(new NbtCompound()));
-		nbt.putInt("price", price);
+		nbt.putUuid(ID_NBT_KEY, id);
+		nbt.putBoolean(IS_ACTIVE_NBT_KEY, isActive);
+		nbt.putBoolean(IS_GENERATED_NBT_KEY, isGenerated);
+		nbt.putUuid(SELLER_ID_NBT_KEY, sellerId);
+		nbt.putString(SELLER_NAME_NBT_KEY, sellerName);
+		nbt.putLong(TIMESTAMP_NBT_KEY, timestamp);
+		nbt.putLong(DURATION_NBT_KEY, duration);
+		nbt.put(STACK_NBT_KEY, stack.writeNbt(new NbtCompound()));
+		nbt.putInt(PRICE_NBT_KEY, price);
+		nbt.putInt(FEES_NBT_KEY, fees);
 
 		return nbt;
 	}
